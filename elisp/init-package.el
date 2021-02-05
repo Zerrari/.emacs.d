619ff71e0cd77e26b023821718fddfa125183891
Zerrari <1660822219@qq.com>
2021-02-05 21:20:27 +0800

    clean code

--- elisp/init-package.el
+++ elisp/init-package.el
@@ -66,9 +66,6 @@
 
 
 
-;; eaf
-;; (add-to-list 'load-path "~/.emacs.d/eaf/")
-;; (require 'eaf)
 
 ;; flycheck
 (use-package flycheck
@@ -93,14 +90,6 @@
   :config
   (which-key-mode))
 
-
-;; aweosome-tab
-;;(load-file "~/.emacs.d/site-elisp/awesome-tab/awesome-tab.el")
-;;(require 'awesome-tab)
-;;(awesome-tab-mode t)
-;;(setq awesome-tab-label-fixed-length 10)
-;;(setq awesome-tab-height 100)
-
 ;; avy
 (use-package avy
   :diminish
@@ -114,10 +103,6 @@
   :diminish
   :config
   (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
-
-;; snails
-;; (add-to-list 'load-path "~/.emacs.d/elpa/")
-;; (require 'snails)
 
 ;; neotree
 (use-package neotree
@@ -153,12 +138,6 @@
   :ensure t
   :config
   (setq figlet-default-font "banner"))
-
-;; popup-kill-ring
-(use-package popup-kill-ring
-  :disabled
-  :bind
-  (("M-y" . popup-kill-ring)))
 
 ;; page-break-lines
 (use-package page-break-lines
@@ -196,14 +175,6 @@
   (keyfreq-autosave-mode 1)
   :bind
   (("M-f" . keyfreq-show)))
-
-;; leetcode
-(use-package leetcode
-  :config
-  (setq leetcode-prefer-language "c")
-  (setq leetcode-directory "~/notes/leetcode")
-  :bind
-  (("M-l" . leetcode)))
 
 ;; multiple-cursors
 (use-package multiple-cursors
