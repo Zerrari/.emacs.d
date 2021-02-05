84ebbecd534a07e576ca76a9db9bac878dbb9d87
Zerrari <1660822219@qq.com>
2021-02-05 20:12:10 +0800

    test

--- init.el
+++ init.el
@@ -45,7 +45,6 @@
  '(package-selected-packages
    '(mood-line anaconda-mode eglot ivy evil-mc evil-nerd-commenter goto-chg iedit selectrum zenburn-theme edit-indirect exec-path-from-shell markdown-mode cl-generic cl-lib hexo indent-guide expand-region smex ace-window quickrun multiple-cursors dired-icon figlet all-the-icons-dired youdao-dictionary yasnippet which-key vterm use-package smartparens rainbow-delimiters popup-kill-ring org-bullets neotree monokai-theme monitor keyfreq hungry-delete flycheck evil-tutor evil-leader dracula-theme doom-themes diminish dashboard cyberpunk-theme cyberpunk-2019-theme counsel company benchmark-init avy airline-themes)))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
