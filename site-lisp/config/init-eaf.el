5806825700863aa15d3df1ec8fec404b68209e94
Zerrari <1660822219@qq.com>
2021-08-19 10:05:56 +0800

    eaf

---
+++ site-lisp/config/init-eaf.el
@@ -0,0 +1,54 @@
+;;; init-eaf.el --- eaf                              -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  zerrari
+
+;; Author: zerrari <zerrari@zhangyizhongdeMacBook-Pro.local>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(require 'eaf)
+
+(require 'eaf-browser)
+(require 'eaf-terminal)
+;; (require 'eaf-mermaid)
+;; (require 'eaf-file-manager)
+;; (require 'eaf-vue-demo)
+;; (require 'eaf-video-player)
+;; (require 'eaf-file-sender)
+;; (require 'eaf-airshare)
+;; (require 'eaf-image-viewer)
+;; (require 'eaf-system-monitor)
+;; (require 'eaf-demo)
+;; (require 'eaf-markdown-previewer)
+;; (require 'eaf-netease-cloud-music)
+;; (require 'eaf-mindmap)
+;; (require 'eaf-file-browser)
+;; (require 'eaf-pdf-viewer)
+;; (require 'eaf-org-previewer)
+;; (require 'eaf-jupyter)
+;; (require 'eaf-music-player)
+;; (require 'eaf-camera)
+
+
+
+(provide 'init-eaf)
+
+;;; init-eaf.el ends here
